3b5d9065ff1d5168877ea19cb5ee85e1d79ebad4
Matt Nichols <mattnichols206@gmail.com>
2023-03-22 12:13:44 -0700

    Add SCSS

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.scss';
+</script>
+
+<div class="wrapper">
+	<slot />
+</div>
+
+<style lang="scss">
+	$test-bg: #f00;
+
+	.wrapper {
+		background-color: $test-bg;
+	}
+</style>
