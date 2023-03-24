ebd06a7e984e88cec465bfc8e56f2e3cc198666c
Matt Nichols <mattnichols206@gmail.com>
2023-03-24 09:24:47 -0700

    Clean up main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import axios from 'axios';
-	import type { ConvergencePair } from '$lib/types';
 	import PairDisplay from '$lib/components/PairDisplay.svelte';
 	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
@@ -32,80 +30,13 @@
 		}
 	);
 
-	// let pairs: ConvergencePair[] = [{ user: '', gpt: '' }];
-	// let nextWord: string | null = data.firstWord;
-	// let pendingSubmission: string | null = null;
-
 	onMount(() => {
 		currentPairDisplay.setFocus();
 	});
 
-	// function requestNextWord(submittedWord: string) {
-	// 	const lastWord = nextWord;
-	// 	axios
-	// 		.get('/api/converge', {
-	// 			params: {
-	// 				word1: nextWord,
-	// 				word2: submittedWord
-	// 			}
-	// 		})
-	// 		.then((res) => {
-	// 			nextWord = res.data;
-	// 			if (pendingSubmission) {
-	// 				submitWord(pendingSubmission);
-	// 				pendingSubmission = null;
-	// 			}
-	// 		})
-	// 		.catch((err) => {
-	// 			console.log(err.message);
-	// 			nextWord = lastWord;
-	// 			errored = true;
-	// 		});
-	// }
-
-	// function retry() {
-	// 	if (!pendingSubmission) throw new Error('No pending submission to retry.');
-	// 	errorDisplayed = false;
-	// 	requestNextWord(pendingSubmission);
-	// }
-
-	// function submitWord(submittedWord: string) {
-	// 	// Make sure we have the nextWord
-	// 	if (!nextWord) {
-	// 		throw new Error('Submission made before next word was ready');
-	// 	}
-
-	// 	// Make the request for the next word
-	// 	requestNextWord(submittedWord);
-
-	// 	// Update & display current state
-	// 	pairs[pairs.length - 1].gpt = nextWord;
-	// 	pairs.push({ user: '', gpt: '' });
-	// 	pairs = pairs;
-	// 	nextWord = null;
-	// 	loading = false;
-	// 	setTimeout(() => {
-	// 		currentPairDisplay.setFocus();
-	// 	}, 0);
-	// }
-
 	function userSubmit(event: CustomEvent<{ word: string }>) {
-		const submittedWord = event.detail.word;
-		gameManager.submitLocalWord(submittedWord);
-
-		// // Check if the previous request failed or is still pending
-		// if (errored) {
-		// 	pendingSubmission = submittedWord;
-		// 	errorDisplayed = true;
-		// 	return;
-		// } else if (!nextWord) {
-		// 	pendingSubmission = submittedWord;
-		// 	loading = true;
-		// 	return;
-		// }
-
-		// // Submit the word!
-		// submitWord(submittedWord);
+		// todo: sanitize input
+		gameManager.submitLocalWord(event.detail.word);
 	}
 </script>
 
