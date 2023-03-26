af0ef398e8b185ac86ef310cdb7178342b3bfbc3
Matt Nichols <mattnichols206@gmail.com>
2023-03-25 17:06:38 -0700

    PostCSS & header beginnings

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 </script>
 
 <div class="wrapper">
+	<div class="header">
+		<h1>Convergence</h1>
+		<h2>(?)</h2>
+	</div>
 	<slot />
 </div>
 
@@ -12,5 +16,20 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+	}
+
+	.header {
+		position: absolute;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		width: 100%;
+		padding: 0.25rem;
+
+		background-image: linear-gradient(
+			rgba(0, 0, 0, 0.75),
+			cubic-bezier(0.25, 0, 0.75, 1),
+			rgba(0, 0, 0, 0)
+		);
 	}
 </style>
